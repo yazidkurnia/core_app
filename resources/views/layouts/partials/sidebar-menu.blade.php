f4e09d645b09dc1cf53821a2f0caec25ddb8dcce
yazidkurnia <zidyazid222@gmail.com>
2026-01-17 15:17:21 +0700

    feat(menu): menu

--- resources/views/layouts/partials/sidebar-menu.blade.php
+++ resources/views/layouts/partials/sidebar-menu.blade.php
@@ -12,7 +12,40 @@
 <li class="{{ Request::is('management/users*') ? 'active' : '' }}">
     <a href="{{ route('manage.users') }}" class="nav-link">
         <i class="fas fa-fire"></i>
-        <span>Pengguna</span>
+        <span>Users</span>
+    </a>
+</li>
+<li class="{{ Request::is('customers/session*') ? 'active' : '' }}">
+    <a href="{{ route('manage.users') }}" class="nav-link">
+        <i class="fas fa-fire"></i>
+        <span>Sessions</span>
+    </a>
+</li>
+
+{{-- Add your menu items here --}}
+<li class="menu-header">Setting</li>
+<li class="{{ Request::is('management/users*') ? 'active' : '' }}">
+    <a href="{{ route('manage.users') }}" class="nav-link">
+        <i class="fas fa-fire"></i>
+        <span>Booth</span>
+    </a>
+</li>
+<li class="{{ Request::is('custpmers/session*') ? 'active' : '' }}">
+    <a href="{{ route('manage.users') }}" class="nav-link">
+        <i class="fas fa-fire"></i>
+        <span>Frames</span>
+    </a>
+</li>
+<li class="{{ Request::is('custpmers/session*') ? 'active' : '' }}">
+    <a href="{{ route('manage.users') }}" class="nav-link">
+        <i class="fas fa-fire"></i>
+        <span>Frame Categories</span>
+    </a>
+</li>
+<li class="{{ Request::is('custpmers/session*') ? 'active' : '' }}">
+    <a href="{{ route('manage.users') }}" class="nav-link">
+        <i class="fas fa-fire"></i>
+        <span>Themes</span>
     </a>
 </li>
 
